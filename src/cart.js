690761f0564024f72baf960363f0eddd99681665
GhostReconxxx <ghostreconxxx10@gmail>
2025-12-24 23:39:39 -1100

    A message will appear in the cart page if the customer cart is empty  and a button to return to the home page.

--- src/cart.js
+++ src/cart.js
@@ -1,4 +1,5 @@
-
+let label = document.getElementById("label")
+let shoppingCart = document.getElementById("shopping-cart")
 let basket = JSON.parse(localStorage.getItem("data")) || []
 let calculation = (basket) =>{
     let cartIcon = document.querySelector(".cartAmount")
@@ -6,5 +7,24 @@ let calculation = (basket) =>{
     
 }
 
+let generateCart = (basket) => {
+
+    if(basket.length !== 0){
+        console.log("Basket is not empty.")
+    }
+    else{
+        shoppingCart.innerHTML = ``
+        label.innerHTML = `
+            <h2>Cart is Empty</h2>
+            <a href="index.html"> 
+                <button class="btn">Back to Home</button>
+            </a>
+        `
+        
+    }
+}
+
 
 calculation(basket)
+
+generateCart(basket)    
